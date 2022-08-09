defa5e542555d853f455e0eeb940af73f00d4318
vianaV19 <matheus.viana518062@gmail.com>
2022-08-08 22:24:55 -0300

    mycommit

---
+++ src/SingletonEager.java
@@ -0,0 +1,11 @@
+public class SingletonEager {
+    private static SingletonEager instance = new SingletonEager();
+
+    private SingletonEager(){
+        super();
+    }
+
+    public static SingletonEager getInstance(){
+        return instance;
+    }
+}
